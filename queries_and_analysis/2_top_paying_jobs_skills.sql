28b37d5a4ebce0e4df9616eace1bc6f4854b8766
pesevivo <pesevivo@gmail.com>
2024-04-08 23:09:03 +0300

    Query 2 ready.

--- queries_and_analysis/2_top_paying_jobs_skills.sql
+++ queries_and_analysis/2_top_paying_jobs_skills.sql
@@ -1,24 +1,37 @@
 /* 
-Question: What skills are required for the top-paying job?
-- Use the obtained 10 top-paying jobs from the first query.
-- Discover the specific skills required for those roles.
+ Question: What skills are required for the top-paying job?
+ - Use the obtained 10 top-paying jobs from the first query.
+ - Discover the specific skills required for those roles.
  */
 
-SELECT
-    jp_fact.job_id,
-    jp_fact.job_title,
-    cp_dim.name AS company_name,
-    jp_fact.job_schedule_type,
-    jp_fact.salary_year_avg,
-    jp_fact.job_posted_date
-FROM
-    job_postings_fact AS jp_fact 
-INNER JOIN company_dim AS cp_dim ON jp_fact.company_id = cp_dim.company_id
-WHERE 
-    job_country = 'Germany' AND
-    job_title_short = 'Data Analyst' AND
-    salary_year_avg IS NOT NULL
-ORDER BY
-    salary_year_avg DESC
-LIMIT
-    10
+ -- CTE
+WITH top_paying_jobs AS (
+    SELECT 
+        jp_fact.job_id,
+        jp_fact.job_title,
+        cp_dim.name AS company_name,
+        jp_fact.salary_year_avg
+    FROM 
+        job_postings_fact AS jp_fact
+        INNER JOIN company_dim AS cp_dim ON jp_fact.company_id = cp_dim.company_id
+    WHERE 
+        job_country = 'Germany'
+        AND job_title_short = 'Data Analyst'
+        AND salary_year_avg IS NOT NULL
+    ORDER BY 
+        salary_year_avg DESC
+    LIMIT 
+        10
+)
+
+SELECT 
+    sk_dim.skills,
+    COUNT(*) AS cnt
+FROM 
+    top_paying_jobs
+    INNER JOIN skills_job_dim AS skjob_dim ON top_paying_jobs.job_id = skjob_dim.job_id
+    INNER JOIN skills_dim AS sk_dim ON skjob_dim.skill_id = sk_dim.skill_id
+GROUP BY
+    sk_dim.skills
+ORDER BY 
+    cnt DESC  
