18464b65a0d26ef83a8ad73428ad851571b68d86
pesevivo <pesevivo@gmail.com>
2024-04-08 23:05:49 +0300

    Query 2 ready.

---
+++ queries_and_analysis/1_top_paying_jobs.sql
@@ -0,0 +1,25 @@
+/* 
+Question: What are the top-paying data analyst jobs?
+- Identify the 10 top-paying data analyst jobs in Germany.
+- Focus is on job postings with disclosed salaries (null values should be removed)
+ */
+
+ SELECT
+    jp_fact.job_id,
+    jp_fact.job_title,
+    cp_dim.name AS company_name,
+    jp_fact.job_schedule_type,
+    jp_fact.salary_year_avg,
+    jp_fact.job_posted_date
+FROM
+    job_postings_fact AS jp_fact 
+    INNER JOIN company_dim AS cp_dim ON jp_fact.company_id = cp_dim.company_id
+WHERE 
+    job_country = 'Germany' AND
+    job_title_short = 'Data Analyst' AND
+    salary_year_avg IS NOT NULL
+ORDER BY
+    salary_year_avg DESC
+LIMIT
+    10
+
---
+++ sql_course.session.sql
@@ -0,0 +1,5 @@
+/* 
+Question: What are the top-paying data analyst jobs?
+- Identify the 10 top-paying data analyst jobs in Germany.
+- Focus is on job postings with disclosed salaries (null values should be removed
+ */
