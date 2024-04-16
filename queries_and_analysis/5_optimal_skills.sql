88c6124b471b5e359446f255d71d30ecf346e011
pesevivo <pesevivo@gmail.com>
2024-04-17 00:01:55 +0300

    -

--- queries_and_analysis/5_optimal_skills.sql
+++ queries_and_analysis/5_optimal_skills.sql
@@ -1,3 +1,10 @@
+/*
+Question: What are the most optimal skills to learn? (high demand and high paying)
+ - Examine the average salary associated for each skill Data Analyst positions.
+ - Focus is on job postings with disclosed salaries and located in Germany. (null values should be removed)
+
+*/
+
 SELECT 
     sk_dim.skill_id,
     sk_dim.skills,
